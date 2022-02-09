7772c5736f7c160f14c4c21ef71a2a5bf682f299
Daniel Juarez <juarezdaniel@gmail.com>
2022-02-09 10:47:55 -0500

    final commit

--- levelthree.py
+++ levelthree.py
@@ -4,7 +4,9 @@ turn = 1
 print('Enter a number between 1 and 10, computer has 3 guesses to win!')
 player = input('1,2,3,4,5,6,7,8,9,10 ')
 play = True
-computer = randint(1, 10)  #computer_number = (min_num + max_num) // 2  this floors and guesses 5 to split that guesses then. This is box section
+min_num = 1
+max_num = 10
+computer = (min_num + max_num) // 2  #computer_number = (min_num + max_num) // 2  this floors and guesses 5 to split that guesses then. This is box section
 #min_num, max_num = 1,10 
 
 while play == True:
@@ -15,8 +17,8 @@ while play == True:
     elif int(player) < computer:
         print(computer, 'Computer guessed High!')
         turn = turn + 1
-        computer = randint(1, computer)
+        max_num = computer
     elif int(player) > computer:
         print(computer, 'Computer guessed Low!')
         turn = turn + 1
-        computer = randint(computer, 10)
+        min_num = computer
